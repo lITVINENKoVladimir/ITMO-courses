f2e49343e33be112487175dc99b833f473fb634c
lITVINENKoVladimir <vov200584@gmail.com>
2024-10-04 16:07:25 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,29 @@
+# todo: Определить в коде переменные:
+# 1. Целочисленного типа
+# 2. Вещественного типа
+# 3. Логического типа
+# 4. Строкового типа
+# 5. Пустого типа
+# Вывести их типы.
+
+# Integer type variable
+integer_variable = 10
+
+# Float type variable
+float_variable = 3.14
+
+# Boolean type variable
+boolean_variable = True
+
+# String type variable
+string_variable = "Hello, world!"
+
+# None type variable (empty type)
+none_variable = None
+
+# Print the types of the variables
+print(type(integer_variable))  # Output: <class 'int'>
+print(type(float_variable))  # Output: <class 'float'>
+print(type(boolean_variable)) # Output: <class 'bool'>
+print(type(string_variable)) # Output: <class 'str'>
+print(type(none_variable))  # Output: <class 'NoneType'>
---
+++ Task2.py
@@ -0,0 +1,29 @@
+# todo: Преобразуйте переменную age и foo в число
+# age = "23"
+# foo = "23abc"
+# num_age= int(age)
+# num_foo= int(foo)v
+
+#
+# Преобразуйте переменную age в Boolean
+# age = "123abc"
+# bool_age = bool(age)
+# print= (f"as boleean: {bool_age}");
+# Преобразуйте переменную flag в Boolean
+# flag = 1
+# flag_bool = bool(flag)
+# print(f"flag as boolean: {flag_bool}")
+# Преобразуйте значение в Boolean
+str_one = "Privet"
+str_two = ""
+bool_str_one = bool(str_one)
+bool_str_two = bool(str_two)
+# Преобразуйте значение 0 и 1 в Boolean
+bool_one = bool(1)
+bool_zero = bool(0)
+print(f"0 as boolean: {bool_zero}")
+print(f"1 as boolean: {bool_one}")
+# Преобразуйте False в строку
+bool_false = False
+str_false = str(bool_false)
+print(f"False as string: {str_false}")
